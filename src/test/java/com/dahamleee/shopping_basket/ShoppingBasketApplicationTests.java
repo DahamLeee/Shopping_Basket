89c9fdae3aec5147739d92bede14a7687d7b51ea
DahamLeee <dlek1011@gmail.com>
2022-06-03 13:54:00 +0900

    chore: first commit

---
+++ src/test/java/com/dahamleee/shopping_basket/ShoppingBasketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dahamleee.shopping_basket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingBasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
