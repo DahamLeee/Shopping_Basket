e8e8b21cca4fc100dbd9bfe8fd5cd95aab790861
DahamLeee <dlek1011@gmail.com>
2022-06-04 00:42:15 +0900

    feat: Custom Exception for CartProduct business logic

---
+++ src/main/java/com/dahamleee/shopping_basket/exception/CartProductCountMinimumException.java
@@ -0,0 +1,8 @@
+package com.dahamleee.shopping_basket.exception;
+
+public class CartProductCountMinimumException extends RuntimeException {
+
+    public CartProductCountMinimumException(String message) {
+        super(message);
+    }
+}
